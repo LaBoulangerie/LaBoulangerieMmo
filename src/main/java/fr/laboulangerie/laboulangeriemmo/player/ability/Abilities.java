3497b47d7238e65e6a15024846cb06c999723ac1
GroobleDierne <grooble.dierne@gmail.com>
2021-08-02 18:48:46 +0200

    Added constructor

--- src/main/java/fr/laboulangerie/laboulangeriemmo/player/ability/Abilities.java
+++ src/main/java/fr/laboulangerie/laboulangeriemmo/player/ability/Abilities.java
@@ -1,19 +1,37 @@
 package fr.laboulangerie.laboulangeriemmo.player.ability;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public enum Abilities {
 
-    FAST_MINE();
+    FAST_MINE(0, TimeUnit.MINUTES, 0, null, new FastMine());
 
 
-    private int requieredLevel;
+    private int requiredLevel;
     private TimeUnit cooldownUnit;
     private int cooldown;
 
     private Class<?> parentTalentClass;
     private AbilityExecutor executor;
 
+    Abilities(
+        int requiredLevel, TimeUnit cooldownUnit,
+        int cooldown, Class<?> parentTalentClass, 
+        AbilityExecutor executor
+    ) {
+        this.requiredLevel = requiredLevel;
+        this.cooldownUnit = cooldownUnit;
+        this.parentTalentClass = parentTalentClass;
+        this.executor = executor;
+    }
 
+    public AbilityExecutor getExecutor() {
+        return this.executor;
+    }
+
+    public static Stream<Abilities> stream() {
+        return Stream.of(Abilities.values());
+    }
 
 }
