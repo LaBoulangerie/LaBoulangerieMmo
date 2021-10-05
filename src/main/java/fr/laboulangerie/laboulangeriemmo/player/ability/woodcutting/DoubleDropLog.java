d1ac28f8aa9415915354d10ce992fb8a0660c829
Pikachuz3 <tinyangel@mail.com>
2021-10-05 18:06:40 +0200

    Fixes
    -All log types supported
    -Item drops at block location not player location anymore
    -better variable names

--- src/main/java/fr/laboulangerie/laboulangeriemmo/player/ability/woodcutting/DoubleDropLog.java
+++ src/main/java/fr/laboulangerie/laboulangeriemmo/player/ability/woodcutting/DoubleDropLog.java
@@ -1,9 +1,9 @@
 package fr.laboulangerie.laboulangeriemmo.player.ability.woodcutting;
 
 import java.util.Random;
+
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
@@ -15,7 +15,7 @@ public class DoubleDropLog extends AbilityExecutor {
 
 	@Override
 	public AbilityTrigger getAbilityTrigger() {
-		return AbilityTrigger.LEFT_CLICK_BLOCK;
+		return AbilityTrigger.BREAK;
 	}
 
 	@Override
@@ -23,32 +23,33 @@ public class DoubleDropLog extends AbilityExecutor {
 		  BlockBreakEvent event = (BlockBreakEvent) baseEvent;
 	      Block block = event.getBlock();
 	      if (event.getBlock().hasMetadata("laboulangerie:placed")) return false;
-	      return block != null && block.getType() == Material.OAK_LOG;
+	      return block != null && block.getType() == Material.ACACIA_LOG  || block.getType() == Material.BIRCH_LOG || block.getType() == Material.DARK_OAK_LOG || block.getType() == Material.JUNGLE_LOG || block.getType() == Material.OAK_LOG || block.getType() == Material.SPRUCE_LOG || block.getType() == Material.CRIMSON_STEM || block.getType() == Material.WARPED_STEM;
 	}
 
 	@Override
 	public void trigger(Event baseEvent, int level) {
 
 		BlockBreakEvent event = (BlockBreakEvent) baseEvent;
-        Player player = event.getPlayer();
-        ItemStack item = new ItemStack(Material.OAK_LOG);
+        Block block = event.getBlock();
+        ItemStack item = new ItemStack(block.getType());
+
         	  
-        int max = 100;
-        int min = 1;
-        Random randomNum = new Random();
-        int r = min + randomNum.nextInt(max);
+        int max_number = 100;
+        int min_number = 1;
+        Random random_chance = new Random();
+        int find_nearest_int = min_number + random_chance.nextInt(max_number);
         	
         if (level >= 60) {
-        	if (r <= 80) {
-        		player.getWorld().dropItemNaturally(player.getLocation(), item);
+        	if (find_nearest_int <= 80) {
+        		block.getWorld().dropItemNaturally(block.getLocation(), item);
         	}
         }else if (level >= 40) {
-        	if (r <= 40) {
-        		player.getWorld().dropItemNaturally(player.getLocation(), item);
+        	if (find_nearest_int <= 40) {
+        		block.getWorld().dropItemNaturally(block.getLocation(), item);
         	}
         }else {
-        	if (r <= 10) {
-        		player.getWorld().dropItemNaturally(player.getLocation(), item);
+        	if (find_nearest_int <= 10) {
+        		block.getWorld().dropItemNaturally(block.getLocation(), item);
         	}
         }
 	}
