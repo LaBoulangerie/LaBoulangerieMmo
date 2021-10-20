e87f94046acb6c9e37c4305f84a2c2585588100f
GroobleDierne <grooble.dierne@gmail.com>
2021-10-20 18:46:46 +0200

    We can now spawn an invisible glowing shulker

--- src/main/java/fr/laboulangerie/laboulangeriemmo/player/ability/mining/MagneticField.java
+++ src/main/java/fr/laboulangerie/laboulangeriemmo/player/ability/mining/MagneticField.java
@@ -1,8 +1,6 @@
 package fr.laboulangerie.laboulangeriemmo.player.ability.mining;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import com.comphenix.protocol.PacketType;
@@ -10,8 +8,8 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher.Registry;
 import com.comphenix.protocol.wrappers.WrappedDataWatcher.WrappedDataWatcherObject;
-import com.comphenix.protocol.wrappers.WrappedWatchableObject;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -43,12 +41,12 @@ public class MagneticField extends AbilityExecutor {
         Player player = event.getPlayer();
         Location location = player.getLocation();
         // https://github.com/libraryaddict/LibsDisguises/blob/master/src/main/java/me/libraryaddict/disguise/utilities/packets/packethandlers/PacketHandlerSpawn.java
-        PacketContainer spawnSlime = new PacketContainer(PacketType.Play.Server.SPAWN_ENTITY_LIVING);
-        StructureModifier<Object> mods = spawnSlime.getModifier();
+        PacketContainer spawnShulker = new PacketContainer(PacketType.Play.Server.SPAWN_ENTITY_LIVING);
+        StructureModifier<Object> mods = spawnShulker.getModifier();
 
         mods.write(0, 1000000) //Entity ID
             .write(1, UUID.randomUUID()) // entity UUID
-            .write(2, 75) // Entity type ID 75 = slime TODO chaange to 80 for 1.17
+            .write(2, 70) // Entity type ID 70 (wiki.vg is saying 69, funny guys...) = shulker TODO chaange to 75 for 1.17
             .write(3, location.getX()) // Pos X, Y, Z
             .write(4, location.getY())
             .write(5, location.getZ())
@@ -59,17 +57,17 @@ public class MagneticField extends AbilityExecutor {
             .write(10, (byte) 0) // pitch
             .write(11, (byte) 0); // yaw
 
-        PacketContainer slimeMetadata = new PacketContainer(PacketType.Play.Server.ENTITY_METADATA);
-        StructureModifier<Object> metadataMods = slimeMetadata.getModifier();
+        PacketContainer shulkerMetadata = new PacketContainer(PacketType.Play.Server.ENTITY_METADATA);
+        StructureModifier<Object> metadataMods = shulkerMetadata.getModifier();
 
-        metadataMods.write(0, 1000000);
-        WrappedDataWatcher newWatcher = new WrappedDataWatcher();
-        WrappedDataWatcher.WrappedDataWatcherObject obj = new WrappedDataWatcherObject(0, new WrappedDataWatcher.Serializer(byte.class, null, false));
-        List<WrappedWatchableObject> watchableObjects = new ArrayList<WrappedWatchableObject>();
-        slimeMetadata.getWatchableCollectionModifier().write(0, watchableObjects);
+        metadataMods.write(0, 1000000); //I already forgot what this do Edit: I remember now it's the entity's Id
 
+        WrappedDataWatcher watcher = new WrappedDataWatcher();
+        watcher.setObject(new WrappedDataWatcherObject(0, Registry.get(Byte.class)),(byte) (0x20 | 0x40)); //0x20 invisible 0x40 glowing
+        shulkerMetadata.getWatchableCollectionModifier().write(0, watcher.getWatchableObjects());
         try {
-            ProtocolLibrary.getProtocolManager().sendServerPacket(player, spawnSlime);
+            ProtocolLibrary.getProtocolManager().sendServerPacket(player, spawnShulker);
+            ProtocolLibrary.getProtocolManager().sendServerPacket(player, shulkerMetadata);
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
