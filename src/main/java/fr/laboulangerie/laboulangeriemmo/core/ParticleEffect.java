de34cdc38f2062d7d7bff384d990fdfb2339fd81
GroobleDierne <grooble.dierne@gmail.com>
2021-10-27 20:29:44 +0200

    Decreased particle effect's size

--- src/main/java/fr/laboulangerie/laboulangeriemmo/core/ParticleEffect.java
+++ src/main/java/fr/laboulangerie/laboulangeriemmo/core/ParticleEffect.java
@@ -13,11 +13,18 @@ public class ParticleEffect {
         Location loc = player.getLocation();
         World world = player.getWorld();
         int radius = 2;
-        for(double y = 0; y <= 50; y+=0.05) {
-            double x = radius * Math.cos(y);
-            double z = radius * Math.sin(y);
-            Particle.DustOptions dustOptions = new Particle.DustOptions(Color.ORANGE, 3);
+        int i = 0;
+        for(double y = 0; y <= 3; y+=0.05) {
+            double x = radius * Math.cos(i*0.1);
+            double z = radius * Math.sin(i*0.1);
+            Particle.DustOptions dustOptions = new Particle.DustOptions(Color.ORANGE, 2);
             world.spawnParticle(Particle.REDSTONE, loc.getX() + x, loc.getY() + y, loc.getZ() + z, 3, 1, 0.6, 0, dustOptions);
+            i++;
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 	}
 }
