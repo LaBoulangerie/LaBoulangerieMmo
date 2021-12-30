efda2865f52c95a8f5d15a1e13c07aee3f1b6ca1
GroobleDierne <grooble.dierne@gmail.com>
2021-12-30 19:15:22 +0100

    🔁Particle runnable is now async

--- src/main/java/fr/laboulangerie/laboulangeriemmo/core/ParticleEffect.java
+++ src/main/java/fr/laboulangerie/laboulangeriemmo/core/ParticleEffect.java
@@ -13,25 +13,21 @@ import fr.laboulangerie.laboulangeriemmo.LaBoulangerieMmo;
 public class ParticleEffect {
     public void createHelix(Player player) {
         new BukkitRunnable() {
+            Location loc = player.getLocation();
+            World world = player.getWorld();
+            int radius = 2;
+            int i = 0;
+            double y = 0;
             @Override
             public void run() {
-                Location loc = player.getLocation();
-                World world = player.getWorld();
-                int radius = 2;
-                int i = 0;
-                for (double y = 0; y <= 3; y += 0.05) {
-                    double x = radius * Math.cos(i * 0.1);
-                    double z = radius * Math.sin(i * 0.1);
-                    Particle.DustOptions dustOptions = new Particle.DustOptions(Color.ORANGE, 2);
-                    world.spawnParticle(Particle.REDSTONE, loc.getX() + x, loc.getY() + y, loc.getZ() + z, 3, 1, 0.6, 0, dustOptions);
-                    i++;
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+                y += 0.05;
+                double x = radius * Math.cos(i * 0.1);
+                double z = radius * Math.sin(i * 0.1);
+                Particle.DustOptions dustOptions = new Particle.DustOptions(Color.ORANGE, 2);
+                world.spawnParticle(Particle.REDSTONE, loc.getX() + x, loc.getY() + y, loc.getZ() + z, 3, 1, 0.6, 0, dustOptions);
+                i++;
+                if (y >= 3) this.cancel();
             }
-        }.runTask(LaBoulangerieMmo.PLUGIN);
+        }.runTaskTimerAsynchronously(LaBoulangerieMmo.PLUGIN, 0, 1);
     }
 }
