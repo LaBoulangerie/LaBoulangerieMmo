b8c729bb478635311ee0458d4a3ef4363628a8dd
Pikachuz3 <tinyangel@mail.com>
2021-12-30 17:08:26 +0100

    variable name change
    changed bar2 to bossbar

--- src/main/java/fr/laboulangerie/laboulangeriemmo/core/Bar.java
+++ src/main/java/fr/laboulangerie/laboulangeriemmo/core/Bar.java
@@ -13,7 +13,7 @@ import fr.laboulangerie.laboulangeriemmo.player.talent.Talent;
 public class Bar {
 	
 	private final LaBoulangerieMmo plugin;
-	public BossBar bar2;
+	public BossBar bossbar;
 	private MmoListener mmoListener;
 	public Bar bar;
 	
@@ -25,18 +25,18 @@ public class Bar {
 	public void createbar(Talent talent, MmoPlayer mmoPlayer) {
 		
 		
-		bar2 = Bukkit.createBossBar(talent.getDisplayName() + " : " + talent.getLevel(0.2) + " | " + (talent.getXp() - talent.getLevelXp(LaBoulangerieMmo.XP_MULTIPLIER)) + "/" + ((int) Math.pow((talent.getLevel(LaBoulangerieMmo.XP_MULTIPLIER) + 1) / LaBoulangerieMmo.XP_MULTIPLIER, 2) - talent.getLevelXp(LaBoulangerieMmo.XP_MULTIPLIER)), BarColor.GREEN, BarStyle.SOLID);
-		bar2.addPlayer(Bukkit.getPlayer(mmoPlayer.getUniqueId()));
+		bossbar = Bukkit.createBossBar(talent.getDisplayName() + " : " + talent.getLevel(0.2) + " | " + (talent.getXp() - talent.getLevelXp(LaBoulangerieMmo.XP_MULTIPLIER)) + "/" + ((int) Math.pow((talent.getLevel(LaBoulangerieMmo.XP_MULTIPLIER) + 1) / LaBoulangerieMmo.XP_MULTIPLIER, 2) - talent.getLevelXp(LaBoulangerieMmo.XP_MULTIPLIER)), BarColor.GREEN, BarStyle.SOLID);
+		bossbar.addPlayer(Bukkit.getPlayer(mmoPlayer.getUniqueId()));
 		Double progress = (talent.getXp() - talent.getLevelXp(LaBoulangerieMmo.XP_MULTIPLIER))/((int) Math.pow((talent.getLevel(LaBoulangerieMmo.XP_MULTIPLIER) + 1) / LaBoulangerieMmo.XP_MULTIPLIER, 2) - talent.getLevelXp(LaBoulangerieMmo.XP_MULTIPLIER));
-		bar2.setProgress(progress);
-		bar2.setVisible(true);
+		bossbar.setProgress(progress);
+		bossbar.setVisible(true);
 		Bukkit.getScheduler().runTaskLater(plugin, () -> {
-			bar2.setVisible(false);
+			bossbar.setVisible(false);
 		}, 60L);
 	}
 	
 	public BossBar getBar() {
-		return bar2;
+		return bossbar;
 	}
 	
 }
