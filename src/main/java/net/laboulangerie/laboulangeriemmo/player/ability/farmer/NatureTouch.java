9b073715282a15efa161ee3058a8e0b7fc1c45fe
GroobleDierne <grooble.dierne@gmail.com>
2022-01-31 19:36:17 +0100

    🧹Code clean up

--- src/main/java/net/laboulangerie/laboulangeriemmo/player/ability/farmer/NatureTouch.java
+++ src/main/java/net/laboulangerie/laboulangeriemmo/player/ability/farmer/NatureTouch.java
@@ -5,14 +5,10 @@ import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.plugin.Plugin;
 
 import net.laboulangerie.laboulangeriemmo.LaBoulangerieMmo;
-import net.laboulangerie.laboulangeriemmo.events.MmoPlayerBreakBlockEvent;
 import net.laboulangerie.laboulangeriemmo.player.ability.AbilityExecutor;
 import net.laboulangerie.laboulangeriemmo.player.ability.AbilityTrigger;
 
@@ -35,37 +31,31 @@ public class NatureTouch extends AbilityExecutor{
 	public void trigger(Event baseEvent, int level) {
 		BlockBreakEvent event = (BlockBreakEvent) baseEvent;
 		final Block block = event.getBlock();
-        Material cropBlockType = null;
-        Material seedBlockType = null;
-        if (block.getType() == Material.WHEAT_SEEDS || block.getType() == Material.WHEAT) {
-          cropBlockType = Material.WHEAT;
-          seedBlockType = Material.WHEAT_SEEDS;
-        } else if (block.getType() == Material.POTATO || block.getType() == Material.POTATOES) {
-          cropBlockType = Material.POTATOES;
-          seedBlockType = Material.POTATO;
-        } else if (block.getType() == Material.CARROT || block.getType() == Material.CARROTS) {
-          cropBlockType = Material.CARROTS;
-          seedBlockType = Material.CARROT;
-        } else if (block.getType() == Material.NETHER_WART) {
-          cropBlockType = Material.NETHER_WART;
-          seedBlockType = Material.NETHER_WART;
-        } else {
-          return;
-        } 
-        Ageable ageable = (Ageable)block.getBlockData();
-        if (ageable.getAge() == 0) {
-          block.getDrops().clear();
-          event.setCancelled(true);
-        } else {
-          final Material finalCropBlockType = cropBlockType;
-          Bukkit.getScheduler().runTaskLater((Plugin)LaBoulangerieMmo.PLUGIN, new Runnable() {
-                public void run() {
-                  block.setType(finalCropBlockType);
-                }
-              },  1L);          
-          } 
-          event.setCancelled(false);
-        
+    Material cropBlockType = null;
+    if (block.getType() == Material.WHEAT_SEEDS || block.getType() == Material.WHEAT) {
+      cropBlockType = Material.WHEAT;
+    } else if (block.getType() == Material.POTATO || block.getType() == Material.POTATOES) {
+      cropBlockType = Material.POTATOES;
+    } else if (block.getType() == Material.CARROT || block.getType() == Material.CARROTS) {
+      cropBlockType = Material.CARROTS;
+    } else if (block.getType() == Material.NETHER_WART) {
+      cropBlockType = Material.NETHER_WART;
+    } else return;
+
+    Ageable ageable = (Ageable)block.getBlockData();
+
+    if (ageable.getAge() == 0) {
+      block.getDrops().clear();
+      event.setCancelled(true);
+    } else {
+
+      final Material finalCropBlockType = cropBlockType;
+      Bukkit.getScheduler().runTaskLater(LaBoulangerieMmo.PLUGIN, new Runnable() {
+        public void run() {
+          block.setType(finalCropBlockType);
         }
-	
+      },  1L);
+    }
+    event.setCancelled(false);
+  }
 }
