16253cb326a0148938bfd9728deb97c5b0b1940f
Blackoutburst <blackoutburst@hotmail.com>
2022-03-23 18:02:06 +0100

    Added ArmorHider packet

---
+++ src/main/java/net/laboulangerie/laboulangeriemmo/core/hiding/ArmorHider.java
@@ -0,0 +1,68 @@
+package net.laboulangerie.laboulangeriemmo.core.hiding;
+
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArmorHider {
+
+    // DO NOT RENAME THEY MATCH MOJANG NAMES
+    private enum EquipmentSlot {
+        MAINHAND,
+        OFFHAND,
+        FEET,
+        LEGS,
+        CHEST,
+        HEAD
+    }
+
+    private static Class<?> getClass(String packageName, String name) {
+        try {
+            return Class.forName(packageName + "." + name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void hideArmor(Player other, Player self) {
+        sendPacket(other, self.getEntityId(), 0, EquipmentSlot.HEAD);
+        sendPacket(other, self.getEntityId(), 0, EquipmentSlot.CHEST);
+        sendPacket(other, self.getEntityId(), 0, EquipmentSlot.LEGS);
+        sendPacket(other, self.getEntityId(), 0, EquipmentSlot.FEET);
+    }
+
+    private static void sendPacket(Player player, int entityID, int itemID, EquipmentSlot slot) {
+        try {
+            final Class<?> packetClass = getClass("net.minecraft.network.protocol.game", "PacketPlayOutEntityEquipment");
+            final Class<?> itemClass = getClass("net.minecraft.world.item", "Item");
+            final Class<?> itemStackClass = getClass("net.minecraft.world.item", "ItemStack");
+            final Class<?> iMaterialClass = getClass("net.minecraft.world.level", "IMaterial");
+            final Class<?> pairClass = getClass("com.mojang.datafixers.util", "Pair");
+            final Class enumClass = getClass("net.minecraft.world.entity", "EnumItemSlot");
+
+            final Enum enumSlot = Enum.valueOf(enumClass, slot.toString());
+
+            final Constructor<?> pairConstructor = pairClass.getConstructor(Object.class, Object.class);
+            final Constructor<?> packetConstructor = packetClass.getConstructor(int.class, List.class);
+            final Constructor<?> itemStackConstructor = itemStackClass.getConstructor(iMaterialClass);
+
+            final Object itemStack = itemStackConstructor.newInstance(itemClass.getMethod("b", int.class).invoke(null, itemID));
+            final Object pair = pairConstructor.newInstance(enumSlot, itemStack);
+
+            final List<Object> equipmentSlots = new ArrayList<>();
+            equipmentSlots.add(pair);
+
+            final Object packet = packetConstructor.newInstance(entityID, equipmentSlots);
+            final Object handle = player.getClass().getMethod("getHandle").invoke(player);
+            final Object playerConnection = handle.getClass().getField("b").get(handle);
+            final Object networkManager = playerConnection.getClass().getField("a").get(playerConnection);
+
+            networkManager.getClass().getMethod("a", getClass("net.minecraft.network.protocol", "Packet")).invoke(networkManager, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
