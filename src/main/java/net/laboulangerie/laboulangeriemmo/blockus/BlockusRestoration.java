b9ccf373fc476cf2cfb9913d8ed53118da5cec3f
GroobleDierne <grooble.dierne@gmail.com>
2022-04-24 14:05:37 +0200

    🗑Drop blockuses when their world is deleted

--- src/main/java/net/laboulangerie/laboulangeriemmo/blockus/BlockusRestoration.java
+++ src/main/java/net/laboulangerie/laboulangeriemmo/blockus/BlockusRestoration.java
@@ -1,5 +1,8 @@
 package net.laboulangerie.laboulangeriemmo.blockus;
 
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.bukkit.scheduler.BukkitRunnable;
 
 import net.laboulangerie.laboulangeriemmo.LaBoulangerieMmo;
@@ -11,7 +14,20 @@ public class BlockusRestoration extends BukkitRunnable {
 
     @Override
     public void run() {
+        AtomicReference<Boolean> warnedWorldRemoval = new AtomicReference<>(false);
         BlockusDataHolder blockusDataHolder = LaBoulangerieMmo.PLUGIN.getBlockusDataManager().getBlockusDataHolder();
-        blockusDataHolder.getBlockuses().forEach(blockus -> blockus.markAsBlockus());
+
+        for (Iterator<Blockus> iterator = blockusDataHolder.getBlockuses().iterator(); iterator.hasNext();) {
+            Blockus blockus = iterator.next();
+            try {
+                blockus.markAsBlockus();
+            } catch (NullPointerException e) {
+                iterator.remove();
+
+                if (warnedWorldRemoval.get()) return; //Warn only on the first failure
+                warnedWorldRemoval.set(true);
+                LaBoulangerieMmo.PLUGIN.getLogger().warning("A world has been deleted so all its blockuses have been dropped.");
+            }
+        }
     }
 }
