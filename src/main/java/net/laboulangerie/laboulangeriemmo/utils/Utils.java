0239fdcc99bca5d0d59dc83558553253601c8a22
GroobleDierne <grooble.dierne@gmail.com>
2023-04-20 22:10:53 +0200

    Fix NMS

--- src/main/java/net/laboulangerie/laboulangeriemmo/utils/Utils.java
+++ src/main/java/net/laboulangerie/laboulangeriemmo/utils/Utils.java
@@ -1,36 +1,28 @@
 package net.laboulangerie.laboulangeriemmo.utils;
 
-import com.google.common.collect.Multimap;
-
 import org.bukkit.craftbukkit.v1_19_R3.inventory.CraftItemStack;
-import org.bukkit.craftbukkit.v1_19_R3.util.CraftNamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import net.minecraft.core.DefaultedRegistry;
+import com.google.common.collect.Multimap;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 
 public class Utils {
     public static double getAttackDamage(Player player, ItemStack itemStack) {
 
-        net.minecraft.world.item.ItemStack craftItemStack = CraftItemStack
-                .asNMSCopy(player.getInventory().getItem(player.getInventory().getHeldItemSlot()));
+        net.minecraft.world.item.ItemStack craftItemStack =
+                CraftItemStack.asNMSCopy(player.getInventory().getItem(player.getInventory().getHeldItemSlot()));
         net.minecraft.world.item.Item item = craftItemStack.getItem();
-        double damages =
-                player.getAttribute(org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE).getValue()
-                        - 7;
-        if (item instanceof net.minecraft.world.item.SwordItem
-                || item instanceof net.minecraft.world.item.TieredItem
+        double damages = player.getAttribute(org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE).getValue() - 7;
+        if (item instanceof net.minecraft.world.item.SwordItem || item instanceof net.minecraft.world.item.TieredItem
                 || item instanceof net.minecraft.world.item.TridentItem) {
 
             Multimap<Attribute, AttributeModifier> attributes =
                     craftItemStack.getItem().getDefaultAttributeModifiers(EquipmentSlot.MAINHAND);
-            Attribute base = DefaultedRegistry.ATTRIBUTE.get(CraftNamespacedKey
-                    .toMinecraft(org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE.getKey()));
-            damages += ((AttributeModifier) attributes.get(base).toArray()[0]).getAmount() + 1;
+            damages += ((AttributeModifier) attributes.get(Attributes.ATTACK_DAMAGE).toArray()[0]).getAmount() + 1;
 
             if (itemStack.getEnchantmentLevel(Enchantment.DAMAGE_ALL) != 0) {
                 damages += 0.5 * itemStack.getEnchantmentLevel(Enchantment.DAMAGE_ALL) + 0.5;
