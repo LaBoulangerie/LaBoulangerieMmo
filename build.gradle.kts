509766d87d0106a60631de8a4f12191349af0fa6
Matteo Planchet <matteo.planchet@gmail.com>
2023-11-20 14:43:23 +0100

    Try to upload to jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.userdev") version "1.5.4"
+    id("io.papermc.paperweight.userdev") version "1.5.9"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
