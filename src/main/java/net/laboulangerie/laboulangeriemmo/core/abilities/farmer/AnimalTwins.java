57f629d40cba1aa86e827264a2a67663e9706f86
Matteo Planchet <matteo.planchet@gmail.com>
2024-04-23 22:35:03 +0200

    Refactor AnimalTwins

--- src/main/java/net/laboulangerie/laboulangeriemmo/core/abilities/farmer/AnimalTwins.java
+++ src/main/java/net/laboulangerie/laboulangeriemmo/core/abilities/farmer/AnimalTwins.java
@@ -1,7 +1,5 @@
 package net.laboulangerie.laboulangeriemmo.core.abilities.farmer;
 
-import java.util.Random;
-
 import org.bukkit.Material;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
@@ -14,6 +12,10 @@ import net.laboulangerie.laboulangeriemmo.api.ability.AbilityArchetype;
 import net.laboulangerie.laboulangeriemmo.api.ability.AbilityExecutor;
 
 public class AnimalTwins extends AbilityExecutor {
+
+    private final static float TIER_3_CHANCE = 1f;
+    private final static float TIER_2_CHANCE = 0.4f;
+    private final static float TIER_1_CHANCE = 0.1f;
 
     public AnimalTwins(AbilityArchetype archetype) {
         super(archetype);
@@ -29,18 +31,17 @@ public class AnimalTwins extends AbilityExecutor {
         EntityBreedEvent event = (EntityBreedEvent) baseEvent;
         Entity entity = event.getEntity();
 
-        Random rng = new Random();
-        double random = rng.nextDouble();
+        float random = (float) Math.random();
 
-        if (level >= getTier(2)
-                || (level >= getTier(1) && random <= 0.4)
-                || (random <= 0.1)) {
-
+        if (level >= getTier(2) && random <= TIER_3_CHANCE
+                || (level >= getTier(1) && random <= TIER_2_CHANCE)
+                || (random <= TIER_1_CHANCE)) {
             if (entity instanceof Sniffer) {
                 ItemStack snifferEgg = new ItemStack(Material.SNIFFER_EGG, 1);
                 entity.getWorld().dropItem(entity.getLocation(), snifferEgg);
                 return;
             }
+
             Animals animal = (Animals) entity.getWorld().spawnEntity(entity.getLocation(), entity.getType());
             animal.setBaby();
         }
