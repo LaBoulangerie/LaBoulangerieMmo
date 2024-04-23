7bfb5d1f76299049887e44c97cd4ffa66e69d104
Matteo Planchet <matteo.planchet@gmail.com>
2024-04-23 22:29:09 +0200

    Fix sniffer twins

--- src/main/java/net/laboulangerie/laboulangeriemmo/core/abilities/farmer/AnimalTwins.java
+++ src/main/java/net/laboulangerie/laboulangeriemmo/core/abilities/farmer/AnimalTwins.java
@@ -2,10 +2,13 @@ package net.laboulangerie.laboulangeriemmo.core.abilities.farmer;
 
 import java.util.Random;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Sniffer;
 import org.bukkit.event.Event;
 import org.bukkit.event.entity.EntityBreedEvent;
+import org.bukkit.inventory.ItemStack;
 
 import net.laboulangerie.laboulangeriemmo.api.ability.AbilityArchetype;
 import net.laboulangerie.laboulangeriemmo.api.ability.AbilityExecutor;
@@ -26,22 +29,19 @@ public class AnimalTwins extends AbilityExecutor {
         EntityBreedEvent event = (EntityBreedEvent) baseEvent;
         Entity entity = event.getEntity();
 
-        int max_number = 100;
-        int min_number = 1;
-        Random random_chance = new Random();
-        int find_nearest_int = min_number + random_chance.nextInt(max_number);
+        Random rng = new Random();
+        double random = rng.nextDouble();
 
-        if (level >= getTier(2)) {
-            Animals animal =
-                    (Animals) entity.getWorld().spawnEntity(entity.getLocation(), entity.getType());
-            animal.setBaby();
-        } else if (level >= getTier(1) && find_nearest_int <= 40) {
-            Animals animal =
-                    (Animals) entity.getWorld().spawnEntity(entity.getLocation(), entity.getType());
-            animal.setBaby();
-        } else if (find_nearest_int <= 10) {
-            Animals animal =
-                    (Animals) entity.getWorld().spawnEntity(entity.getLocation(), entity.getType());
+        if (level >= getTier(2)
+                || (level >= getTier(1) && random <= 0.4)
+                || (random <= 0.1)) {
+
+            if (entity instanceof Sniffer) {
+                ItemStack snifferEgg = new ItemStack(Material.SNIFFER_EGG, 1);
+                entity.getWorld().dropItem(entity.getLocation(), snifferEgg);
+                return;
+            }
+            Animals animal = (Animals) entity.getWorld().spawnEntity(entity.getLocation(), entity.getType());
             animal.setBaby();
         }
 
